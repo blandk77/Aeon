c425fbdb4a6d1bc8f54dd612b27a935893781c66
blandk77 <alexags1983@gmail.com>
2025-03-05 19:34:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # REQUIRED CONFIG
-BOT_TOKEN = "7477897274:AAHqoYa4zVAqhjWXWYXyVtxgZIM1Gfx8Mos"
-OWNER_ID =1705634892
-TELEGRAM_API = 26728872
-TELEGRAM_HASH = "96985c2aaea6c75408528909b7e18879"
+BOT_TOKEN = ""
+OWNER_ID = 0
+TELEGRAM_API = 0
+TELEGRAM_HASH = ""
 
 # SEMI-REQUIRED, WE SUGGEST TO FILL IT FROM MONGODB
-DATABASE_URL = "mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse"
+DATABASE_URL = ""
 
 # OPTIONAL CONFIG
 TG_PROXY = {}
@@ -13,14 +13,14 @@ USER_SESSION_STRING = ""
 DOWNLOAD_DIR = "/usr/src/app/downloads/"
 CMD_SUFFIX = ""
 AUTHORIZED_CHATS = ""
-SUDO_USERS = "1705634892"
+SUDO_USERS = ""
 DEFAULT_UPLOAD = "rc"
 FILELION_API = ""
 STREAMWISH_API = ""
 EXCLUDED_EXTENSIONS = ""
-INCOMPLETE_TASK_NOTIFIER = True
+INCOMPLETE_TASK_NOTIFIER = False
 YT_DLP_OPTIONS = ""
-USE_SERVICE_ACCOUNTS = True
+USE_SERVICE_ACCOUNTS = False
 NAME_SUBSTITUTE = ""
 FFMPEG_CMDS = {}
 UPLOAD_PATHS = {}
@@ -34,8 +34,10 @@ PAID_CHANNEL_LINK = ""
 SET_COMMANDS = True
 METADATA_KEY = ""
 WATERMARK_KEY = ""
-LOG_CHAT_ID = -1002372386341
+LOG_CHAT_ID = 0
 LEECH_FILENAME_CAPTION = ""
+HYDRA_IP = ""
+HYDRA_API_KEY = ""
 
 # GDrive Tools
 GDRIVE_ID = ""
@@ -55,6 +57,28 @@ RCLONE_SERVE_PASS = ""
 MEGA_EMAIL = ""
 MEGA_PASSWORD = ""
 
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    },
+]
+
 # Update
 UPSTREAM_REPO = "https://github.com/blandk77/Aeon"
 UPSTREAM_BRANCH = "main"
@@ -66,7 +90,7 @@ MEDIA_GROUP = False
 USER_TRANSMISSION = False
 HYBRID_LEECH = False
 LEECH_FILENAME_PREFIX = ""
-LEECH_DUMP_CHAT = "-1002467562142"
+LEECH_DUMP_CHAT = ""
 THUMBNAIL_LAYOUT = ""
 
 # qBittorrent/Aria2c
